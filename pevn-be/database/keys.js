7bffffd30cb27bac7ed847ae49b3dc0ad30a9e50
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-11 12:45:09 -0300

    hasta aaqui hice andar la base de datos con la de heroku

--- pevn-be/database/keys.js
+++ pevn-be/database/keys.js
@@ -1,11 +1,27 @@
 import {Pool} from 'pg';
 
 const pool = new Pool({
-    host: 'localhost',
+    host: 'ec2-54-164-40-66.compute-1.amazonaws.com',
+    port: '5432',
+    user: 'zgkqofjqtgnrsi',
+    password: 'f1e71084889cdf0a4276ba221f7075d064e966f03470361165cdc52130f32119',
+    database: 'd3i2cfoh9ce702',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+
+
+module.exports = pool;
+
+
+/*
+
+ host: 'localhost',
     port: '5432',
     user: 'postgres',
     password: 'ferrari1947',
     database: 'pevn'
-});
 
-module.exports = pool;
+*/
